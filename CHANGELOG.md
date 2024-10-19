22bcba026337fa9d09a482da063d9cc70ae49226
Isha Dijcks <ishadijcks@gmail.com>
2024-10-20 01:26:07 +0200

    First release!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @123ishatest/content-parser
 
+## 1.0.1
+
+### Patch Changes
+
+- Fix imports
+
 ## 1.0.0
 
 ### Patch Changes
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { ContentParser } from '@/ContentParser.ts';
 export type { ContentError } from '@/ContentError.ts';
 export type { ContentParserConfigInput, ContentParserConfig } from '@/ContentParserConfig.ts';
 export type { ParseResult } from '@/ParseResult.ts';
+export { ContentParser } from '@/ContentParser.ts';
